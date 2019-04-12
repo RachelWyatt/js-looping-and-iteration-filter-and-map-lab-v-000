d9b19c7fe7ea19ccfdb1abb49bc3b79e5bf738ef
Rachel Wyatt <rw126@evansville.edu>
2019-04-12 14:58:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+  let findByRevenue = drivers.filter(function (revenue) { return 1 > 0 });
+  deepIterator(findByRevenue)
+}
+
+function driverNamesWithRevenueOver(driver, revenue) {
+  
+}
+
+function exactMatch(driver, object) {
+  
+}
+
+function exactMatchToList(driver, object) {
+  
+}
+
+function deepIterator (target) {
+  if (typeof target === 'object') {
+    for (const key in target) {
+      deepIterator(target[key]);
+    }
+  } else {
+    console.log(target);
+  }
+}
